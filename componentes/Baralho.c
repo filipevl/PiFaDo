a7679da072a46eb7362c3219dd75e52a01b82df5
filipevl <filipe.viana@ufv.br>
2021-04-06 18:21:21 -0300

    função gerarNaipe

--- componentes/Baralho.c
+++ componentes/Baralho.c
@@ -6,3 +6,26 @@ typedef struct sBaralho
     int carta;
     char naipe[10];
 } Baralho;
+
+char *gerarNaipe(int k)
+{
+    switch (k)
+    {
+    case 0:
+        return "espada";
+        break;
+    case 1:
+        return "paus";
+        break;
+    case 2:
+        return "copas";
+        break;
+    case 3:
+        return "ouro";
+        break;
+    default:
+        return "";
+        break;
+    }
+}
+
