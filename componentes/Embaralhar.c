6a57dff64af924499c691684eda777394e14d9df
oliveiramiro2 <ramiromares95@gmail.com>
2021-04-09 12:56:31 -0300

    funcao para embaralhar o vetor de cartas

---
+++ componentes/Embaralhar.c
@@ -0,0 +1,36 @@
+#include <time.h>
+#include <conio.h>
+#include "Baralho.c"
+
+#define TAM_BARALHO 52
+
+int existe_indice(int indices_inclusos[52], int valor, int total){
+	int i;
+	for(i=0; i<total; i++){
+		if(indices_inclusos[i]==valor){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void gerar_numero_aleatorio(Baralho *baralho){
+	int i, indices[TAM_BARALHO], num;
+	
+	srand(time(NULL));
+	for(i=0; i<TAM_BARALHO; i++){
+		
+		num = rand() % TAM_BARALHO;
+		printf("\n%d", num);
+		while(existe_indice(indices, num ,i)){
+			num = rand() % TAM_BARALHO;
+		}
+		indices[i] = num;
+		
+	}
+	for(i=0; i<TAM_BARALHO; i++){
+		printf("\nindice: %d", indices[i]);		
+	}
+}
+
+
