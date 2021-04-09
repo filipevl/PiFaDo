6da2d85bec35bf269ee8f2785dd248271d9d9911
leandroUFV <leandroguioliveira@hotmail.com>
2021-04-09 13:04:40 -0300

    inicializar e VAzia estrutura pilha

--- estruturas/Pilha.c
+++ estruturas/Pilha.c
@@ -4,4 +4,18 @@ typedef struct sPilha
 {
     Carta info;
     struct sPilha *cima;
+    int tam;
 } Pilha;
+
+void inicializar(Pilha* pilha) {
+    pilha->cima = NULL;
+    pilha->tam = 0;
+}
+
+int pilhaVazia(Pilha* pilha) {
+    if(pilha->cima == NULL)
+        return 1;
+
+    return 0;
+}
+
