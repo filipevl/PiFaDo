93f867853ee30d52f03fe5568ea759217f87c90d
oliveiramiro2 <ramiromares95@gmail.com>
2021-04-09 12:23:44 -0300

    ajustando index do teste

--- Test/index.c
+++ Test/index.c
@@ -1,8 +1,10 @@
-#include "../componentes/Baralho.c"
+//#include "../componentes/Baralho.c"
+//#include "../componentes/Embaralhar.c"
 
 void imprimirBaralho(Baralho baralho) // teste da função novo baralho
 {
-    for (short i = 0; i < TAM_BARALHO; i++)
+	int i;
+    for (i = 0; i < TAM_BARALHO; i++)
     {
         printf("carta: %d naipe:%s\n", baralho.cartas[i].carta, baralho.cartas[i].naipe);
     }
@@ -11,6 +13,7 @@ void imprimirBaralho(Baralho baralho) // teste da função novo baralho
 void main()
 {
     Baralho baralho1 = novoBaralho(); 
+    //gerar_numero_aleatorio(&baralho1);
     imprimirBaralho(baralho1); // teste da função novo baralho
     system("pause");
 }
