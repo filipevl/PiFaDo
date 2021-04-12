c077d791625236703fdae85288707b7f95507e8b
leandroUFV <leandroguioliveira@hotmail.com>
2021-04-12 10:03:18 -0300

    pilha esrtatica para a trinca

--- estruturas/Pilha.c
+++ estruturas/Pilha.c
@@ -1,21 +1,52 @@
 #include "../componentes/Carta.c"
 
-typedef struct sPilha
-{
-    Carta info;
-    struct sPilha *cima;
-    int tam;
+#define MAX_TAM 3
+
+typedef struct sPilha {
+    Carta info[MAX_TAM];
+    int topo;
 } Pilha;
 
 void inicializar(Pilha* pilha) {
-    pilha->cima = NULL;
-    pilha->tam = 0;
+    pilha->topo=-1;
 }
 
 int pilhaVazia(Pilha* pilha) {
-    if(pilha->cima == NULL)
+    if(pilha->topo==-1)
         return 1;
 
     return 0;
 }
+
+int pilhaCheia(Pilha* pilha) {
+    if(pilha ->topo == (MAX_TAM-1))
+        return 1;
+
+    return 0;
+}
+
+
+int push(Pilha* pilha, Carta carta) {
+    if(pilhaCheia(pilha))
+        return 0;
+	
+	pilha->topo++;
+    pilha->info[pilha->topo] = carta;
+    
+    return 1;
+}
+
+Carta *pop(Pilha* pilha) {
+
+
+    if(pilhaVazia(pilha))
+        return NULL;
+        
+	pilha->info[pilha->topo].carta=0;
+	strcpy(pilha->info[pilha->topo].naipe,"");
+	pilha->topo--;   
+ 	
+    return NULL;
+}
+
 
