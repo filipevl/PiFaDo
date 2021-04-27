a0d4db3de5c03c4750886f6d51635f1cd925b1f3
oliveiramiro2 <ramiromares95@gmail.com>
2021-04-27 02:20:03 -0300

    ajustes nas verificações e incluir do descarte de cartas no monte

--- componentes/Jogo.c
+++ componentes/Jogo.c
@@ -41,75 +41,115 @@ void jogo(){
     	char *valor_carta; // mostra o valor da carta
     	int jogada; // variavel que decide acao do jogador
     	
-    	printf("\nJogador %d\nDigite 1 para tirar a carta do monte principal \nDigite 2 para tirar do seu espaco\n\n", jogador);
+    	printf("\n       Jogador %d\nDigite 1 para tirar a carta do monte principal \nDigite 2 para tirar do seu espaco\n\n", jogador);
     	scanf("%d", &jogada);
     	
-    	if(jogada == 1){
-    		//jogador tira a primeira carta do seu respectivo monte
+    	if(jogada == 1){ // jogador tira a primeira carta do seu respectivo monte
     		if(jogador == 1){
     			mostra_valor(monte1, &valor_carta);
     			printf("\nJogador 1 a carta virada foi: %s de %s\n", valor_carta, monte1->inicio->carta_fila.naipe);
     			//logica...
-    			do{
+    			do{ // escolha de qual movimento o jogador vai fazer
     				printf("\nDigite 1 para inserir nos espacos 1, 2, 3, 4 \nDigite 2 para inserir no espaco x1  \nDigite 3 para descartar no fim do baralho\n\n");
     			    scanf("%d", &jogada);
 				}while(jogada<1 && jogada>3);
 				
     			if(jogada == 1){ // inserir em algum espaço e1,e2,e3,e4
-    				do{
+    				do{ // escolha de qual espaco ele vai inserir 1, 2, 3 ou 4
     					printf("\nDigite 1 inserir espaco 1 \nDigite 2 inserir espaco 2 \nDigite 3 inserir espaco 3 \nDigite 4 inserir espaco 4\n\n");
     		    	    scanf("%d", &jogada);
 					}while(jogada<1 && jogada>4);
     				
     				if(jogada == 1){ // inserir espaço 1
-    				    
-    				    inserir_carta_espaco(&espaco1_1, &monte1);	
+    				    if(inserir_carta_espaco(&espaco1_1, &monte1)){
+    				    	pontos_jogador1++;
+    				    	printf("\nParabens jogador 1 voce marcou 1 ponto!");
+						}	
 					}
-					
 					if(jogada == 2){ // inserir espaço 2
-    					inserir_carta_espaco(&espaco2_1, &monte1);
+						if(inserir_carta_espaco(&espaco2_1, &monte1)){
+    				    	pontos_jogador1++;
+    				    	printf("\nParabens jogador 1 voce marcou 1 ponto!");
+						}
 					}
 					if(jogada == 3){ // inserir espaço 3
-    					inserir_carta_espaco(&espaco3_1, &monte1);
+    					if(inserir_carta_espaco(&espaco3_1, &monte1)){
+    				    	pontos_jogador1++;
+    				    	printf("\nParabens jogador 1 voce marcou 1 ponto!");
+						}
 					}
 					if(jogada == 4){ // inserir espaço 4
-    					inserir_carta_espaco(&espaco4_1, &monte1);
+    					if(inserir_carta_espaco(&espaco4_1, &monte1)){
+    				    	pontos_jogador1++;
+    				    	printf("\nParabens jogador 1 voce marcou 1 ponto!");
+						}
 					}
+					
+					if(pontos_jogador1 == 5){ // verificando se o jogador 1 vendeu o jogo
+						loop_jogo = 0; // parada do loop do jogo
+						printf("\n\nParabens jogador 1 voce vendeu o jogo\n\n");
+					}
+				}else if(jogada == 2){
+					// inserir no espaco x1 ... logica
+				}else if(jogada == 3){ // caso o jogador deseje descartar a carta tirada
+					enfileirar(&monte1, monte1->inicio->carta_fila.carta, monte1->inicio->carta_fila.naipe);
+					desenfileirar(&monte1);	
 				}
-    			printf("\nTurno acabado\n");
+    			printf("\nTurno acabado\n"); // fim do turno jogador 1
 			}
 			
 			if(jogador == 2){ // turno jogador 2
 				mostra_valor(monte2, &valor_carta);
     			printf("\nJogador 2 a carta virada foi: %s de %s\n", valor_carta, monte2->inicio->carta_fila.naipe);
     			//logica....
-    			do{
+    			do{ // logica de decisao de qual movimento o jogador vai fazer
     				printf("\nDigite 1 para inserir nos espacos 1, 2, 3, 4 \nDigite 2 para inserir no espaco x1  \nDigite 3 para descartar no fim do baralho\n\n");
     			    scanf("%d", &jogada);
 				}while(jogada<1 && jogada>3);
 				
     			if(jogada == 1){ // inserir em algum espaço e1,e2,e3,e4
-    				do{
+    				do{ // escolha de qual espaco ele vai inserir 1, 2, 3 ou 4
     					printf("\nDigite 1 inserir espaco 1 \nDigite 2 inserir espaco 2 \nDigite 3 inserir espaco 3 \nDigite 4 inserir espaco 4\n\n");
     		    	    scanf("%d", &jogada);
 					}while(jogada<1 && jogada>4);
     				
     				if(jogada == 1){ // inserir espaço 1
-    				    inserir_carta_espaco(&espaco1_2, &monte2);
+    				    if(inserir_carta_espaco(&espaco1_2, &monte2)){
+    				    	pontos_jogador2++;
+    				    	printf("\nParabens jogador 2 voce marcou 1 ponto!");
+						}
 					}
-					
 					if(jogada == 2){ // inserir espaço 2
-    					inserir_carta_espaco(&espaco2_2, &monte2);
+    					if(inserir_carta_espaco(&espaco2_2, &monte2)){
+    				    	pontos_jogador2++;
+    				    	printf("\nParabens jogador 2 voce marcou 1 ponto!");
+						}
 					}
-					
 					if(jogada == 3){ // inserir espaço 3
-    					inserir_carta_espaco(&espaco3_2, &monte2);
+    					if(inserir_carta_espaco(&espaco3_2, &monte2)){
+    				    	pontos_jogador2++;
+    				    	printf("\nParabens jogador 2 voce marcou 1 ponto!");
+						}
 					}
 					if(jogada == 4){ // inserir espaço 4
-    					inserir_carta_espaco(&espaco4_2, &monte2);
+    					if(inserir_carta_espaco(&espaco4_2, &monte2)){
+    				    	pontos_jogador2++;
+    				    	printf("\nParabens jogador 2 voce marcou 1 ponto!");
+						}
 					}
+				}else if(jogada == 2){
+					// inserir no espaco x1 ... logica
+				}else if(jogada == 3){ // caso o jogador deseje descartar a carta tirada 
+					enfileirar(&monte2, monte2->inicio->carta_fila.carta, monte2->inicio->carta_fila.naipe);
+					desenfileirar(&monte2);
 				}
-    			printf("\nTurno acabado\n");
+				
+				if(pontos_jogador2 == 5){ // verificando se o jogador 1 vendeu o jogo
+					loop_jogo = 0; // parada do loop do jogo
+					printf("\n\nParabens jogador 2 voce vendeu o jogo\n\n");
+				}
+				
+    			printf("\nTurno acabado\n"); // fim do turno do jogador 2
 			}
 			
 			
@@ -127,6 +167,11 @@ void jogo(){
 				jogador = 1;
 			}
 		}
+	}
+	if(pontos_jogador1 == 5){ // se o jogador 1 tiver 5 pontos ele ganha o jogo
+		printf("\n\nParabens jogador 1 voce ganhou o jogo.");
+	}else{ // se o jogador 2 tem 5 pontos ele ganha o jogo
+		printf("\n\nParabens jogador 2 voce vendeu o jogo");
 	}
 }
 
