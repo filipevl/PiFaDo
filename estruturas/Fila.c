f0552065b46a8d8302e5a3646247b9ed472f8bb4
filipevl <filipe.viana@ufv.br>
2021-04-29 01:30:22 -0300

    atualizações e correções na estrutura fila

--- estruturas/Fila.c
+++ estruturas/Fila.c
@@ -1,87 +1,118 @@
 #include "../componentes/Baralho.c"
 
-typedef struct sNO{
+typedef struct sNO
+{
 	Carta carta_fila;
-    struct sNO *prox;
-}NO;
+	struct sNO *prox;
+} NO;
 
-typedef struct sFila{
+typedef struct sFila
+{
 	NO *inicio;
 	NO *fim;
-}Fila;
+} Fila;
 
-void iniciar_fila(Fila **fila){
+void iniciar_fila(Fila **fila)
+{
 	(*fila)->inicio = NULL;
 	(*fila)->fim = NULL;
 }
 
-void alocar_carta(NO **no){
-	*no = (NO*)malloc(sizeof(NO));
+void alocar_carta(NO **no)
+{
+	*no = (NO *)malloc(sizeof(NO));
 }
 
-void alocar_fila(Fila **fila){
-	*fila = (Fila*)malloc(sizeof(Fila));
+void alocar_fila(Fila **fila)
+{
+	*fila = (Fila *)malloc(sizeof(Fila));
 	return;
 }
 
-void desalocar_carta(NO **no){
+void desalocar_carta(NO **no)
+{
 	free(*no);
 	*no = NULL;
 }
 
-void desalocar_fila(Fila **fila){
+void desalocar_fila(Fila **fila)
+{
 	free(*fila);
 	*fila = NULL;
 }
 
-int fila_vazia(Fila *fila){
-	if(fila->fim == NULL && fila->inicio == NULL){
+int fila_vazia(Fila *fila)
+{
+	if (fila->fim == NULL && fila->inicio == NULL)
+	{
 		return 1;
 	}
 	return 0;
 }
 
-void enfileirar(Fila **fila, int valor_carta, char naipe[10]){
+void enfileirar(Fila **fila, int valor_carta, char naipe[10])
+{
 	NO *novo;
 	alocar_carta(&novo);
-	
-	if(novo != NULL){
+
+	if (novo != NULL)
+	{
 		novo->carta_fila.carta = valor_carta;
 		strcpy(novo->carta_fila.naipe, naipe);
 		novo->prox = NULL;
-		if(fila_vazia(*fila)){
+		if (fila_vazia(*fila))
+		{
 			(*fila)->inicio = novo;
 			(*fila)->fim = novo;
-		}else{
+		}
+		else
+		{
 			(*fila)->fim->prox = novo;
 			(*fila)->fim = novo;
 		}
-	}else{
+	}
+	else
+	{
 		printf("\nErro na alocacao do no.");
 	}
 }
 
-void desenfileirar(Fila **fila){
+Carta desenfileirar(Fila **fila)
+{
 	NO *aux;
-	
+	Carta carta;
+
 	alocar_carta(&aux);
-	
-	if(aux != NULL){
-		if(fila_vazia(*fila)){
+
+	if (aux != NULL)
+	{
+		if (fila_vazia(*fila))
+		{
 			printf("\nFila vazia.");
-		}else{
-			if((*fila)->inicio != (*fila)->fim){
+			return;
+		}
+		else
+		{
+			carta = (*fila)->inicio->carta_fila;
+			if ((*fila)->inicio != (*fila)->fim)
+			{
 				aux = (*fila)->inicio->prox;
 				desalocar_carta(&(*fila)->inicio);
 				(*fila)->inicio = aux;
-				return;
 			}
-			desalocar_fila(fila);
-			alocar_fila(fila);
-			iniciar_fila(fila);	
-		}
-	}else{
-		printf("\nErro na alocacao do no.");
-    }
-}
+			else
+			{
+				desalocar_fila(fila);
+				alocar_fila(fila);
+				iniciar_fila(fila);
+			}
 
+			return carta;
+		}
+	}
+	else
+	{
+		printf("\nErro na alocacao do no.");
+		return;
+	}
+}
