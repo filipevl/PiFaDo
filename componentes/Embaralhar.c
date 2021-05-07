50a19487c3b37aaddb4376a34546c540ff84bdb5
oliveiramiro2 <ramiromares95@gmail.com>
2021-05-07 10:07:46 -0300

    Resolvendo ultimos bug nos codigo para ele funcionar perfeitamente

--- componentes/Embaralhar.c
+++ componentes/Embaralhar.c
@@ -6,14 +6,13 @@ int gerar_fila_baralho(Fila **fila, Baralho *baralho, int *ordem)
 {
 	int i;
 	
+	alocar_fila(fila);
 	iniciar_fila(fila);
 
-	for (i = 0; i < TAM_BARALHO; i++)
-	{
-		if (!enfileirar(fila,
-						baralho->cartas[ordem[i]].carta,
-						baralho->cartas[ordem[i]].naipe))
+	for (i = 0; i < TAM_BARALHO; i++){
+		if (!enfileirar(fila, baralho->cartas[ordem[i]].carta, baralho->cartas[ordem[i]].naipe)){
 			return 0;
+		}	
 	}
 	return 1;
 }
@@ -32,22 +31,20 @@ int existe_indice(int *indices_inclusos, int valor, int total)
 	return 0;
 }
 
-int gerar_baralho_aleatorio(Baralho *baralho, Fila **fila)
-{
+int gerar_baralho_aleatorio(Baralho *baralho, Fila **fila){
 	int i, num, indices[TAM_BARALHO];
 
 	srand(time(NULL));
-	for (i = 0; i < TAM_BARALHO; i++)
-	{
+	for (i = 0; i < TAM_BARALHO; i++){
 
 		num = rand() % TAM_BARALHO;
-		while (existe_indice(indices, num, i))
-		{
+		while (existe_indice(indices, num, i)){
 			num = rand() % TAM_BARALHO;
 		}
 		indices[i] = num;
 	}
-	if(gerar_fila_baralho(fila, baralho, indices))
+	if(gerar_fila_baralho(fila, baralho, indices)){
 		return 1;
+	}	
 	return 0;
 }
