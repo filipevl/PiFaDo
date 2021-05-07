d885d9467148ebe7bd0247b6571665692c8dc9f5
oliveiramiro2 <ramiromares95@gmail.com>
2021-05-07 10:10:47 -0300

    Resolvendo ultimos bug nos codigo para ele funcionar perfeitamente

--- estruturas/Fila.c
+++ estruturas/Fila.c
@@ -1,70 +1,47 @@
 #include "../componentes/Baralho.c"
 
-typedef struct sNO
-{
+typedef struct sNO{
 	Carta carta_fila;
 	struct sNO *prox;
 } NO;
 
-typedef struct sFila
-{
+typedef struct sFila{
 	NO *inicio;
 	NO *fim;
 } Fila;
 
-void alocar_carta(NO **no)
-{
+void alocar_carta(NO **no){
 	*no = (NO *)malloc(sizeof(NO));
 }
 
-void alocar_fila(Fila **fila)
-{
+void alocar_fila(Fila **fila){
 	*fila = (Fila *)malloc(sizeof(Fila));
 	return;
 }
 
-void desalocar_carta(NO **no)
-{
+void desalocar_carta(NO **no){
 	free(*no);
 	*no = NULL;
 }
 
-void desalocar_fila(Fila **fila)
-{
+void desalocar_fila(Fila **fila){
 	free(*fila);
 	*fila = NULL;
 }
 
-int iniciar_fila(Fila **fila)
-{
-	if ((*fila) == 0)
-		(*fila) = malloc(sizeof(Fila));
+void iniciar_fila(Fila **fila){
+	(*fila)->inicio = NULL;
+	(*fila)->fim = NULL;
+}
 
-	if ((*fila) != 0)
-	{
-		(*fila)->inicio = NULL;
-		(*fila)->fim = NULL;
+int fila_vazia(Fila *fila){
+	if(fila->fim == NULL && fila->inicio == NULL){
 		return 1;
 	}
 	return 0;
 }
 
-int fila_vazia(Fila *fila)
-{
-	if (fila == 0)
-	{
-		return 1;
-	}
-	else
-	{
-		if (fila->fim == 0 && fila->inicio == 0)
-			return 1;
-	}
-	return 0;
-}
-
-int enfileirar(Fila **fila, int valor_carta, char *naipe)
-{
+int enfileirar(Fila **fila, int valor_carta, char *naipe){
 	NO *novo;
 	alocar_carta(&novo);
 
@@ -92,41 +69,34 @@ int enfileirar(Fila **fila, int valor_carta, char *naipe)
 	return 0;
 }
 
-Carta desenfileirar(Fila **fila)
-{
+Carta desenfileirar(Fila **fila){
 	NO *aux;
 	Carta carta_removida;
 
 	alocar_carta(&aux);
 
-	if (aux != 0)
-	{
-		if (fila_vazia(*fila))
-		{
+	if (aux != 0){
+		if (fila_vazia(*fila)){
 			printf("\nFila vazia.\n");
 			getchar();
 		}
-		else
-		{
+		else{
 			carta_removida.carta = (*fila)->inicio->carta_fila.carta;
 			strcpy(carta_removida.naipe, (*fila)->inicio->carta_fila.naipe);
 
-			if ((*fila)->inicio != (*fila)->fim)
-			{
+			if ((*fila)->inicio != (*fila)->fim){
 				aux = (*fila)->inicio->prox;
 				free((*fila)->inicio);
 				(*fila)->inicio = aux;
 			}
-			else
-			{
+			else{
 				free((*fila)->inicio);
 				(*fila)->inicio = 0;
 				(*fila)->fim = (*fila)->inicio;
 			}
 		}
 	}
-	else
-	{
+	else{
 		printf("\nErro na alocacao do no.");
 		exit(1);
 	}
