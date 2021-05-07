63d1d8cbf1d1e29b38574687a771545b201885ca
oliveiramiro2 <ramiromares95@gmail.com>
2021-05-07 10:06:18 -0300

    Resolvendo ultimos bug nos codigo para ele funcionar perfeitamente

--- componentes/Baralho.c
+++ componentes/Baralho.c
@@ -3,18 +3,15 @@
 #include "Carta.c"
 
 #define TAM_BARALHO 52
-typedef struct sBaralho
-{
+typedef struct sBaralho{
     Carta cartas[TAM_BARALHO];
     int indice;
 } Baralho;
 
-Baralho novoBaralho()
-{
+Baralho novoBaralho(){
     short i, count, carta;
     Baralho baralho;
-    for (i = 0, count = 0, carta=1; i < TAM_BARALHO; i++, count++, carta++)
-    {
+    for (i = 0, count = 0, carta=1; i < TAM_BARALHO; i++, count++, carta++){
         baralho.indice = i;
         strcpy(baralho.cartas[i].naipe, gerarNaipe(count));
         baralho.cartas[i].carta = carta;
